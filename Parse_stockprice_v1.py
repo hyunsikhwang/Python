7b874ae6b340a327a74e3e5d0b441b13c4ab81a6
hyunsikhwang <gomting@naver.com>
2016-06-14 00:27:53 +0900

    Update Parse_stockprice_v1.py

--- Parse_stockprice_v1.py
+++ Parse_stockprice_v1.py
@@ -8,24 +8,14 @@ import pprint
 #processing start
 start_time = time.time()
 
-url = "http://finance.daum.net/quote/all.daum?type=S&stype=Q"
-f = urllib2.urlopen(url)
-page = f.read().decode('utf-8', 'ignore')
-f.close()
+url_P = "http://finance.daum.net/quote/all.daum?type=S&stype=P"  #type : U(업종순), S(가나다순)
+url_Q = "http://finance.daum.net/quote/all.daum?type=S&stype=Q"  #stype : P(유가증권), Q(코스닥)
 
-soup = BeautifulSoup(page)
-
-editData_table = soup.find('table', {'class' : "gTable clr"})
-editData_title = editData_table.findAll("tr")
-#editData_title = editData_title.find_next("tr")
-#editData_title = editData_title.find_next("tr")
-#editData_title = editData_title.find_next("tr")
-#editData_title = editData_title.find("td")
-#editData_title = editData_title.find_next("td")
-
-i = 0
-
+global D
 D = {}
+
+CollectPrices(url_P)
+CollectPrices(url_Q)
 
 class MyPrettyPrinter(pprint.PrettyPrinter):
     def format(self, _object, context, maxlevels, level):
@@ -36,27 +26,47 @@ class MyPrettyPrinter(pprint.PrettyPrinter):
             return "'%s'" % _object.encode('utf8'), True, False
         return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
 
-for li in editData_title:
-    editData_rec = li.findAll('td')
-    for li2 in editData_rec:
-        soup2 = BeautifulSoup(str(li2))
-        #print i, soup2
-        
-        if soup2.string <> None:
-            i = i + 1
-            if soup2.find('a') <> None:
-                temp2 = soup2.find('a')['href']
-                temp2 = temp2[-6:]
-            if i % 3 == 1:
-                temp = soup2.string
-            elif i % 3 == 2:
-                temp3 = soup2.string
-            elif i % 3 == 0:
-                temp4 = soup2.string
-                D[temp] = {temp2, temp3, temp4}
-                #print D
+def CollectPrices(url):
+    f = urllib2.urlopen(url)
+    page = f.read().decode('utf-8', 'ignore')
+    f.close()
 
-MyPrettyPrinter().pprint(D)
+    soup = BeautifulSoup(page)
+
+    editData_table = soup.find('table', {'class' : "gTable clr"})
+    editData_title = editData_table.findAll("tr")
+
+    i = 0
+
+    for li in editData_title:
+        editData_rec = li.findAll('td')
+        for li2 in editData_rec:
+            soup2 = BeautifulSoup(str(li2))
+            #print i, soup2
+
+            if soup2.string <> None:
+                i = i + 1
+                if soup2.find('a') <> None:
+                    temp2 = soup2.find('a')['href']
+                    temp2 = temp2[-6:]
+                if i % 3 == 1:
+                    temp = soup2.string
+                elif i % 3 == 2:
+                    temp3 = soup2.string
+                elif i % 3 == 0:
+                    temp4 = soup2.string
+                    D[temp] = {temp2, temp3, temp4}
+                    #print D
+
+MyPrettyPrinter().pprint(D[u'리노공업'])
+MyPrettyPrinter().pprint(D[u'한미반도체'])
+MyPrettyPrinter().pprint(D[u'미창석유'])
+MyPrettyPrinter().pprint(D[u'동서'])
+MyPrettyPrinter().pprint(D[u'코텍'])
+MyPrettyPrinter().pprint(D[u'금화피에스시'])
+MyPrettyPrinter().pprint(D[u'토비스'])
+MyPrettyPrinter().pprint(D[u'KODEX 레버리지'])
+MyPrettyPrinter().pprint(D[u'KODEX 인버스'])
 #print MyPrettyPrinter().pformat(D)
             
 #processing end
@@ -64,8 +74,4 @@ end_time = time.time()
 
 #print processing time
 print end_time - start_time
-
-#editDataStr = str(editData_title)
-
-#print editDataStr
 
