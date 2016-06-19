72c3ec453802c1a7061430db84e90a438ef52a6c
hyunsikhwang <gomting@naver.com>
2016-06-19 22:11:52 +0900

    Update parse_1.py

--- parse_1.py
+++ parse_1.py
@@ -27,9 +27,9 @@ def CollectPrices(url):
     soup = BeautifulSoup(page, 'html.parser', from_encoding='utf-8')
 
     editData_table = soup.find('ul', {'class' : "list stockrate"})
-    editData_title = editData_table.findAll("li")
+    #editData_title = editData_table.findAll("li")
     
-    print editData_title
+    print editData_table
 
 CollectPrices(url_quote)
 
