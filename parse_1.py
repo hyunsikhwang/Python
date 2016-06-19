681c7241399c76ed9b309b167005b19350124753
hyunsikhwang <gomting@naver.com>
2016-06-19 22:07:41 +0900

    Update parse_1.py

--- parse_1.py
+++ parse_1.py
@@ -1,1 +1,41 @@
+#-*- coding: utf-8 -*-
+import urllib2
+from bs4 import BeautifulSoup
+import time
+import pprint
 
+#processing start
+start_time = time.time()
+
+url_quote = "view-source:http://finance.daum.net/item/main.daum?code=058470"  # 종목 시세 주소
+
+
+class MyPrettyPrinter(pprint.PrettyPrinter):
+    def format(self, _object, context, maxlevels, level):
+        if isinstance(_object, unicode):
+            return "'%s'" % _object.encode('utf8'), True, False
+        elif isinstance(_object, str):
+            _object = unicode(_object,'utf8')
+            return "'%s'" % _object.encode('utf8'), True, False
+        return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
+
+def CollectPrices(url):
+    f = urllib2.urlopen(url)
+    page = f.read().decode('utf-8', 'ignore')
+    f.close()
+
+    soup = BeautifulSoup(page, 'html.parser', from_encoding='utf-8')
+
+    editData_table = soup.find('ul', {'class' : "list stockrate"})
+    editData_title = editData_table.findAll("li")
+    
+    print editData_title
+
+CollectPrices(url_quote)
+
+
+#processing end
+end_time = time.time()
+
+#
+print end_time - start_time
