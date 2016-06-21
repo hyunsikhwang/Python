3688980c82263e3c82f53ab7fb8fe1f2ddacd893
hyunsikhwang <gomting@naver.com>
2016-06-21 23:14:10 +0900

    Update parse_nv.py

--- parse_nv.py
+++ parse_nv.py
@@ -13,29 +13,17 @@ def CollectPrices(url):
     page = f.read().decode('euc-kr', 'ignore')
     f.close()
     js = json.loads(page)
-    print js['result']['areas'][0]['datas'][0]['nv']
-    print js['result']['areas'][0]['datas'][0]['nm']
-    return js
+    print js['result']['areas'][0]['datas'][0]['nv'], js['result']['areas'][0]['datas'][0]['nm']
 
-StockInfo = CollectPrices(url_quote+"058470")
-
-StockInfo = CollectPrices(url_quote + "058470")
-
-StockInfo = CollectPrices(url_quote + "042700")
-
-StockInfo = CollectPrices(url_quote + "003650")
-
-StockInfo = CollectPrices(url_quote + "026960")
-
-StockInfo = CollectPrices(url_quote + "052330")
-
-StockInfo = CollectPrices(url_quote + "036190")
-
-StockInfo = CollectPrices(url_quote + "051360")
-
-StockInfo = CollectPrices(url_quote + "122630")
-
-StockInfo = CollectPrices(url_quote + "114800")
+CollectPrices(url_quote + "058470")
+CollectPrices(url_quote + "042700")
+CollectPrices(url_quote + "003650")
+CollectPrices(url_quote + "026960")
+CollectPrices(url_quote + "052330")
+CollectPrices(url_quote + "036190")
+CollectPrices(url_quote + "051360")
+CollectPrices(url_quote + "122630")
+CollectPrices(url_quote + "114800")
 
 #processing end
 end_time = time.time()
