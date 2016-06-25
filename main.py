d1f271c1142b6d02296e889c3517df8e2e07b381
hyunsikhwang <gomting@naver.com>
2016-06-26 00:34:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def CollectQuote(url):
                     stock_name = li2.text
                     pos = str(li2).find("code=") + 5
                     stock_code = str(li2)[pos:(pos+6)]
-                    #set_stocklist(stock_name.encode('utf-8'), stock_code)
+                    set_stocklist(stock_name.encode('utf-8'), stock_code)
 
 
 #                    name2code = CompList.get_or_insert(stock_name.encode('utf-8'))
@@ -180,7 +180,7 @@ def set_status(chat_id, cmd_status):
     cs.put()
 
 def set_stocklist(s_name, s_code):
-    code2name = QuoteList.get_or_insert(str(s_code))
+    #code2name = QuoteList.get_or_insert(str(s_code))
     code2name.quote_code = s_code
     code2name.quote_name = s_name
     code2name.put()
