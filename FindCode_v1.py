c91edd2389292c143dbf50f19565b850ad131c73
hyunsikhwang <gomting@naver.com>
2016-06-28 23:38:12 +0900

    Update FindCode_v1.py

--- FindCode_v1.py
+++ FindCode_v1.py
@@ -7,7 +7,7 @@ APIKey = "CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zA
 
 def FindCodeAPI(APIKey, stock_name):
   url = 'http://api.seibro.or.kr/openapi/service/StockSvc/getStkIsinByNm'
-  queryParams = '?' + urlencode({ quote_plus('ServiceKey') : APIKey, quote_plus('secnNm') : stock_name, quote_plus('pageNo') : '2', quote_plus('numOfRows') : '2' })
+  queryParams = '?' + urlencode({ quote_plus('ServiceKey') : APIKey, quote_plus('secnNm') : stock_name, quote_plus('pageNo') : '1', quote_plus('numOfRows') : '10' })
 
   request = Request(url + queryParams)
   request.get_method = lambda: 'GET'
