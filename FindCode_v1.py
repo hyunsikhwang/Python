42eea98523cf98d6946fe63c261f2e2d7c954771
hyunsikhwang <gomting@naver.com>
2016-06-28 23:53:50 +0900

    Update FindCode_v1.py

--- FindCode_v1.py
+++ FindCode_v1.py
@@ -16,7 +16,7 @@ def FindCodeAPI(APIKey, stock_name):
   
   #print page
 
-  soup = BeautifulSoup(page, from_encoding='utf-8')
+  soup = BeautifulSoup(page, 'html.parser', from_encoding='utf-8')
   editData_list = soup.findAll('korSecnNm')
   
   print editData_list
