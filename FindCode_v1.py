077419d73886801f8176cb1e55fa7ccda80bf509
hyunsikhwang <gomting@naver.com>
2016-06-28 23:48:27 +0900

    Update FindCode_v1.py

--- FindCode_v1.py
+++ FindCode_v1.py
@@ -17,7 +17,7 @@ def FindCodeAPI(APIKey, stock_name):
   print page
 
   soup = BeautifulSoup(page, 'html.parser', from_encoding='utf-8')
-  editData_list = soup.findAll('secnKacdNm').text
+  editData_list = soup.findAll('secnKacdNm')
   
   for editData_name in editData_list:
     print editData_name
