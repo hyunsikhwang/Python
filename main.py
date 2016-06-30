5ea1ffc9bfd814aa3305d23152866257d483cb9a
hyunsikhwang <gomting@naver.com>
2016-06-30 20:34:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -421,7 +421,7 @@ def process_cmds(msg):
         return
     if get_status(chat_id) == ST_ADD:
         result_list = FindCodeAPI(APIKey, text)
-        if is_empty(result_list):
+        if len(result_list) == 0:
             return
         else:
             cmd_addquote(chat_id, result_list[0][0], result_list[0])
