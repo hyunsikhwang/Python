caf18d647ff5dc859cbe9c76a4781cffe11c381c
hyunsikhwang <gomting@naver.com>
2016-07-02 00:02:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -439,6 +439,7 @@ def process_cmds(msg):
         else:
             merge_list = MergeList(result_list[0])
             result_list[0].append([CMD_NONE])
+            result_list[0].append([str(len(result_list[0]))])
             cmd_addquote(chat_id, merge_list, result_list[0])
         return
     if get_status(chat_id) == ST_DEL:
