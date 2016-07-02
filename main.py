db88e76d11a3ca0dfcbf617dbe81b49def800928
hyunsikhwang <gomting@naver.com>
2016-07-03 00:28:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -437,6 +437,9 @@ def process_cmds(msg):
         elif len(result_list[0]) == 1 and result_list[0][0][0] == text:
             send_msg(chat_id, result_list[0][0][0] + u' 종목이 추가되었습니다.', keyboard=CUSTOM_KEYBOARD)
         else:
+            for li in result_list[0]:
+                if li[0] == text:
+                    send_msg(chat_id, u'동일한 종목이 발견되었습니다.')
             merge_list = MergeList(result_list[0])
             result_list[0].append([CMD_NONE])
             cmd_addquote(chat_id, merge_list, result_list[0])
