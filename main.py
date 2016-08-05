15620b0028c28220526b1773cc58cd59e16786d8
hyunsikhwang <gomting@naver.com>
2016-08-05 16:24:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -580,7 +580,8 @@ def cmd_view(chat_id):
     s = CollectIndex(url_index + 'KOSPI')
     s = s + CollectIndex(url_index + 'KOSDAQ')
     for aaa in quote_list:
-        s += CollectPrices(url_quote + aaa)
+        send_msg(chat_id, aaa)
+#        s += CollectPrices(url_quote + aaa)
 #    s = s + CollectPrices(url_quote + '058470')
 #    s = s + CollectPrices(url_quote + '042700')
 #    s = s + CollectPrices(url_quote + '003650')
