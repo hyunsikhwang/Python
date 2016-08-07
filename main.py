6bb2567e13e0f64da244e7aebe9e17ed7e16745a
hyunsikhwang <gomting@naver.com>
2016-08-08 00:18:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -598,7 +598,7 @@ def cmd_reord_execute(chat_id, text):
     text   : (char)    바꾸려고 하는 위치(숫자)
     """
     sl = StockList.get_by_id(str(chat_id))
-    on = OrderNumber.get_by_id(str(chat_id)))
+    on = OrderNumber.get_by_id(str(chat_id))
     oldrank = on.ordnum
     newrank = int(text)
     sltemp = sl.info
