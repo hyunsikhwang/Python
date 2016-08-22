6492d39b22f33fc1df934a84fbe3b4e63b9b2de1
hyunsikhwang <gomting@naver.com>
2016-08-23 00:42:36 +0900

    Update parse_div.py

--- parse_div.py
+++ parse_div.py
@@ -1,8 +1,13 @@
-import lxml.html
+import lxml.etree
 
 url_KSP ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&stkTpcd=1&listTpcd=11&rankTpcd=1&year=2015'
 url_KSQ ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&stkTpcd=1&listTpcd=12&rankTpcd=1&year=2015'
 
-html = lxml.html.parse(url_KSQ)
+response = requests.get(url)
 
-print html
+# Use `lxml.etree` rathern than `lxml.html`, 
+# and unicode `response.text` instead of `response.content`
+doc = lxml.etree.fromstring(response.text)
+
+for path in doc.xpath('//shotnIsin[@name="026960"]/@value'):
+     print path
