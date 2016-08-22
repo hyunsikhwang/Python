028770c1c644b63db5e69ff1174695ed6c6ae6b8
hyunsikhwang <gomting@naver.com>
2016-08-23 00:29:03 +0900

    Update parse_div.py

--- parse_div.py
+++ parse_div.py
@@ -1,1 +1,8 @@
+import lxml.html
 
+url_KSP ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&stkTpcd=1&listTpcd=11&rankTpcd=1&year=2015'
+url_KSQ ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&stkTpcd=1&listTpcd=12&rankTpcd=1&year=2015'
+
+html = lxml.html.parse(url)
+
+print html
