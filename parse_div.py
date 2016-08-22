6ab3368f7c9866ac9528509f5dc40a4465eb427d
hyunsikhwang <gomting@naver.com>
2016-08-23 00:44:22 +0900

    Update parse_div.py

--- parse_div.py
+++ parse_div.py
@@ -1,9 +1,10 @@
 import lxml.etree
+from urllib2 import Request, urlopen
 
 url_KSP ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&stkTpcd=1&listTpcd=11&rankTpcd=1&year=2015'
 url_KSQ ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&stkTpcd=1&listTpcd=12&rankTpcd=1&year=2015'
 
-response = requests.get(url)
+response = Request.get(url)
 
 # Use `lxml.etree` rathern than `lxml.html`, 
 # and unicode `response.text` instead of `response.content`
