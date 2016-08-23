2ed06defa31b8ebb0c68728f31efa476ef985ca2
hyunsikhwang <gomting@naver.com>
2016-08-23 21:44:23 +0900

    Update parse_div.py

--- parse_div.py
+++ parse_div.py
@@ -7,8 +7,9 @@ url_KSP ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?Servi
 url_KSQ ='http://api.seibro.or.kr/openapi/service/StockSvc/getDividendRank?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&stkTpcd=1&listTpcd=12&rankTpcd=1&year=2015'
 
 fp = urllib2.urlopen(url_KSP)
-fp += urllib2.urlopen(url_KSQ)
 doc = etree.parse(fp)
+fp = urllib2.urlopen(url_KSQ)
+doc = doc + fp
 fp.close()
 stocklist = ['058470', '026960', '042700', '003650', '052330', '036190', '114090', '114090', '034230']
 
