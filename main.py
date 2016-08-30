3581e4f89b982cbd3c2bd0ca331b2fe3598c6be0
hyunsikhwang <gomting@naver.com>
2016-08-31 00:07:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -781,8 +781,8 @@ def cmd_div(chat_id):
         
         for sm in stocklist:
             if stockcode[0] == sm:
-                result_text = str(stockcode[0]) + str(stockname[0]) + str(stockdiv[0])
-                send_msg(chat_id, result_text.encode('utf-8'))
+                result_text = str(stockcode[0]).encode('utf-8') + str(stockname[0]).encode('utf-8') + str(stockdiv[0]).encode('utf-8')
+                send_msg(chat_id, result_text)
 
 
 def cmd_broadcast(chat_id, text):
